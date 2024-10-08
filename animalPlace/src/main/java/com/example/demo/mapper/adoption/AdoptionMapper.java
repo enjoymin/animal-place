33e4bec280071a089757710da6e5115d8725521d
bgj5593 <bgj5593@naver.com>
2024-10-08 16:49:01 +0900

    stray push

---
+++ animalPlace/src/main/java/com/example/demo/mapper/adoption/AdoptionMapper.java
@@ -0,0 +1,9 @@
+package com.example.demo.mapper.adoption;
+
+import com.example.demo.model.adoption.AdoptionDTO;
+
+public interface AdoptionMapper {
+
+	int insertAdoption(AdoptionDTO adoption);
+
+}
