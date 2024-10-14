2fda44eb021c7f0994885488f9bcfd04a124729a
ohgoo4198@naver.com <ohgoo4198@naver.com>
2024-10-14 14:21:14 +0900

    수정

--- animalPlace/src/main/java/com/example/demo/controller/he/BoardController.java
+++ animalPlace/src/main/java/com/example/demo/controller/he/BoardController.java
@@ -1,24 +1,18 @@
 package com.example.demo.controller.he;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.domain.he.Criteria;
-import com.example.demo.service.pboard.PFileService;
+
 
 @Controller
 @RequestMapping("/board/*")
 public class BoardController {
 	
-	@Autowired
-	private PFileService pfservice;
-	
-	
 	@GetMapping("list")
-	public void list(Criteria cri,Model model) {
-		model.addAttribute("flist",pfservice.getBestImage());
+	public void list(Criteria cri) {
+		
 	}
 }
